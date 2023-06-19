eb87436961d0a2dcf898d1ab8caa8bad5f93c8ae
tatenodev <gd28.c.tateno@gmail.com>
2023-06-19 19:46:40 +0900

    cors

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,16 @@
 import { fastify } from "fastify";
 import { fastifyConnectPlugin } from "@bufbuild/connect-fastify";
 import routes from "./connect";
+import cors from "@fastify/cors";
 
 async function main() {
   const server = fastify();
   await server.register(fastifyConnectPlugin, {
     routes,
+  });
+  await server.register(cors, {
+    // put your options here
+    origin: "http://localhost:3000",
   });
   server.get("/", (_, reply) => {
     reply.type("text/plain");
