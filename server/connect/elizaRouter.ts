b01386fe0047e35df968560445925a9aaec3dea5
tatenodev <gd28.c.tateno@gmail.com>
2023-06-24 20:13:49 +0900

    add new user table

--- server/connect/elizaRouter.ts
+++ server/connect/elizaRouter.ts
@@ -7,14 +7,14 @@ export default (router: ConnectRouter) =>
   router.service(ElizaService, {
     // implements rpc Say
     async say(req) {
-      const allUsers = await prisma.user.findMany();
+      const allUsers = await prisma.userOld.findMany();
       return {
         sentence: `You said: ${req.sentence}, users: ${allUsers.length}`,
       };
     },
     async createUser(req) {
       try {
-        const user = await prisma.user.create({
+        const user = await prisma.userOld.create({
           data: {
             name: req.name,
             email: req.email,
