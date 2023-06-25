76f9a875000e6f2d3e7137c907c6fbfb6f6ce3bd
tatenodev <gd28.c.tateno@gmail.com>
2023-06-25 13:00:37 +0900

    wip

--- client/src/functions/auth0/token/accessToken.ts
+++ client/src/functions/auth0/token/accessToken.ts
@@ -11,33 +11,10 @@ const options = {
   headers: { "content-type": "application/x-www-form-urlencoded" },
   data: new URLSearchParams({
     grant_type: "client_credentials",
-    client_id: "TTECnfvjA399aKRgSYVHeQQfdXiSNodU",
-    client_secret: "M3wCQycn-qd_-KcNu1gJGZq6JVHH97yJSeifG2EoOD_DP0MuXKiMVWNEXQ4KrxeG",
+    client_id: process.env.AUTH0_CLIENT_ID ?? "",
+    client_secret: process.env.AUTH0_CLIENT_SECRET ?? "",
     audience: "http://localhost:8080/",
   }),
-  // etudeAPI Local
-  // data: new URLSearchParams({
-  //   grant_type: "client_credentials",
-  //   client_id: "eCxu0Pg19FEg9BMnQDJq2UbSZlg4xXKq",
-  //   client_secret: "PkALU0ayAmHo-zhT1OEkgZoS6-A3rnBzRMB1wHhleJ-6MXF2SuYkk9LIs_tFlQlC",
-  //   audience: "http://localhost:8080/",
-  // }),
-  // headers: { "content-type": "application/json" },
-  // body: `
-  // {
-  //   "client_id":"eCxu0Pg19FEg9BMnQDJq2UbSZlg4xXKq",
-  //   "client_secret":"PkALU0ayAmHo-zhT1OEkgZoS6-A3rnBzRMB1wHhleJ-6MXF2SuYkk9LIs_tFlQlC",
-  //   "audience":"http://localhost:3000/",
-  //   "grant_type":"client_credentials"
-  // }`,
-  //
-  // body: `
-  // {
-  //   "client_id":"TTECnfvjA399aKRgSYVHeQQfdXiSNodU",
-  //   "client_secret":"M3wCQycn-qd_-KcNu1gJGZq6JVHH97yJSeifG2EoOD_DP0MuXKiMVWNEXQ4KrxeG",
-  //   "audience":"http://localhost:8080/",
-  //   "grant_type":"client_credentials"
-  // }`,
 };
 
 export const getAccessToken = async () => {
@@ -52,43 +29,3 @@ export const getAccessToken = async () => {
     });
   return result;
 };
-
-// var request = require("request");
-
-// var options = { method: 'POST',
-//   url: 'https://dev-2dclmx2vfoeqh2di.us.auth0.com/oauth/token',
-//   headers: { 'content-type': 'application/json' },
-//   body: '{"client_id":"6KFM2VWyxZX1UMbnPg8Hcm0WjpXKUF01","client_secret":"_Z-mhZZesjrN9tXZrVWAqWe9l6x7tPKF4VVEO9WATHPhjzDaXJmSAJCObe8IeDoH","audience":"http://localhost:8080/","grant_type":"client_credentials"}' };
-
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
-
-//   console.log(body);
-// });
-
-// import axios from "axios";
-
-// const options = {
-//   method: "POST",
-//   url: "https://{yourDomain}/oauth/token",
-//   headers: { "content-type": "application/x-www-form-urlencoded" },
-//   data: new URLSearchParams({
-//     grant_type: "client_credentials",
-//     client_id: "YOUR_CLIENT_ID",
-//     client_secret: "YOUR_CLIENT_SECRET",
-//     audience: "YOUR_API_IDENTIFIER",
-//   }),
-// };
-
-// const getAccessToken = async () => {
-//   const result = await axios
-//     .request(options)
-//     .then(function (response) {
-//       console.log(response.data);
-//       return response.data;
-//     })
-//     .catch(function (error) {
-//       console.error(error);
-//     });
-//   return result;
-// };
--- server/middlware/verifyToken.ts
+++ server/middlware/verifyToken.ts
@@ -6,9 +6,6 @@ const client = jwksClient({
   jwksUri: "https://dev-2dclmx2vfoeqh2di.us.auth0.com/.well-known/jwks.json",
 });
 
-// const Gtoken =
-//   "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIiwiaXNzIjoiaHR0cHM6Ly9kZXYtMmRjbG14MnZmb2VxaDJkaS51cy5hdXRoMC5jb20vIn0..O17rrAyOBNINGvLE.QEdcdpad1POHYduM9DECzfX6xHKL79QvuPVFGjhP7quw-faqyiBriyPblpjM3tF8waT-t7RbWGmTpyF1HZe7hmVcgvcWJy3TMt6Eln-hduy9M0x8MiVPPyPRx1JQNX6L2O7nF_rsi_l7agYk6Mq4yYftYIsYGHsLCIsgZcp2P503dlh5rWjoRtBBTfFv1_ULpTCO478ECN309xvu5yA1BvO1cgTR02OBr2fwSr9-aB2usSCCt-QLHgkOxZ15Y2d5xbmEMqDIfaMcfKZKDMbYQpEc8zoBUoJjSsxTeggkQ3yUf4UUWg3e9dKz6tDAf2gICfc1Ck2nMJq7k4qa6xixjnojLqrZUgk.MriGYt9rYz7x52-YXseudQ";
-
 // 公開鍵を取得する関数
 function getKey(header, callback) {
   client.getSigningKey(header.kid, function (err, key) {
@@ -16,14 +13,6 @@ function getKey(header, callback) {
     callback(null, signingKey);
   });
 }
-// function getKey(header: { kid: string }, callback) {
-//   client.getSigningKey(header.kid, function (err, key) {
-//     const publicKey = key?.getPublicKey();
-//     // const signingKey = key?.publicKey || key.rsaPublicKey;
-//     const signingKey = publicKey || key.rsaPublicKey;
-//     callback(null, signingKey);
-//   });
-// }
 
 export async function verifyJWT(token: string, callback: (decoded: any, err?: jwt.VerifyErrors) => void) {
   jwt.verify(token, getKey, function (err, decoded) {
@@ -35,24 +24,4 @@ export async function verifyJWT(token: string, callback: (decoded: any, err?: jw
       console.log("Decoded JWT:", decoded);
     }
   });
-  //
-  //
-  // // jwt.verify(token, getKey, (err, decoded) => {
-  // const secret = (await client.getKeys()) as { kid: string };
-
-  // jwt.verify(token, secret.kid, (err, decoded) => {
-  //   if (err) {
-  //     console.log("verifyJWT err!!:", err);
-  //     return;
-  //     // return callback(null, err);
-  //   }
-
-  //   // デコードされたトークンからクレームを取得
-  //   // const { sub, name, permissions } = decoded;
-  //   console.log("decoded", decoded);
-
-  //   // クレームを使用して必要な処理を行う
-
-  //   // callback(decoded);
-  // });
 }
