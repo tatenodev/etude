3ea0a60a62186a332ae359dc23e476fcc0b71fea
tatenodev <gd28.c.tateno@gmail.com>
2023-07-01 12:35:46 +0900

    session調整

--- client/src/app/api/session-login/route.ts
+++ client/src/app/api/session-login/route.ts
@@ -1,21 +1,22 @@
 import { cookies } from "next/headers";
 import { firebaseAdminAuth } from "@/functions/firebase/firebaseAdminConfig";
-import { NextResponse } from "next/server";
 
 export async function POST(request: Response) {
-  // Tokenの有効期限
-  const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5日
-  // const cookieStore = cookies();
-  // const idToken = cookieStore.get("idToken")?.value ?? "";
+  // Tokenの有効期限(秒指定で5日)
+  const expiresIn = 24 * 60 * 60 * 5;
   const { idToken } = await request.json();
-  console.log("server側 idToken:", idToken);
   // Cookieに保存するセッションIDを作成する
-  const sessionCookie = await firebaseAdminAuth().createSessionCookie(idToken, { expiresIn });
+  const sessionCookie = await firebaseAdminAuth().createSessionCookie(idToken, { expiresIn: expiresIn * 1000 });
   const body = JSON.stringify({ status: "success" });
 
-  // TODO: NextResponse使ってみても良いかも
-  return new Response(body, {
-    status: 200,
-    headers: { "Set-Cookie": `session=${sessionCookie}; Max-Age=${expiresIn}; Path=/; HttpOnly` },
+  cookies().set({
+    name: "session",
+    value: sessionCookie,
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    maxAge: expiresIn,
   });
+
+  return new Response(body, { status: 200 });
 }
