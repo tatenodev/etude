7b738db08ed1d632e9ca221e3089bc319a1a6dc5
tatenodev <gd28.c.tateno@gmail.com>
2023-07-01 13:59:17 +0900

    リダイレクト処理

--- client/src/app/talk/page.tsx
+++ client/src/app/talk/page.tsx
@@ -1,11 +1,17 @@
+import { firebaseAdminAuth } from "@/functions/firebase/firebaseAdminConfig";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { Header } from "@/app/_features/Header";
 import { Talk } from "./features/Talk";
 import { Eliza } from "./features/Eliza";
 
 export default async function Dashboard() {
   const cookieStore = cookies();
-  console.log("cookieStore:", cookieStore.get("session"));
+  const auth = firebaseAdminAuth();
+  const idToken = cookieStore.get("session")?.value || "";
+  const user = await auth.verifySessionCookie(idToken, true).catch(() => null);
+
+  if (!user) redirect("/login");
 
   return (
     <>
